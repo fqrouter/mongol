849540ace4eee82b3ed870bdaf778a2c2423c776
Fen Qin <fqrouter@gmail.com>
2013-02-14 16:29:11 +0800

    find router should continue probing until all jamming methods found

--- mongol/config.py
+++ mongol/config.py
@@ -1,10 +1,13 @@
-sniffer_type = 'L2'
-min_ttl = 6
+sniffer_type = 'L2' # can be L3 or Tcpdump, but L2 is working more reliable
+min_ttl = 4
 max_ttl = 14
 debug = True
-fixed_route = None # ('venet0:0', 'a.b.c.d')
+fixed_route = None
+# uncomment below if you have broken routing table which caused the detected outgoing ip or interface is wrong
+#fixed_route = ('venet0:0', 'a.b.c.d')
 interval_between_poke_and_peek = 2
 batch_size = 4
+output_dir = 'var'
 dns_wrong_answer_probe = {
     'sport': 19841,
     'dport': 53
@@ -19,12 +22,15 @@ dns_tcp_rst_probe = {
     'dport': 53,
     'interval_between_syn_and_dns_question': 0.5
 }
-tcp_packet_drop_probe = {
-    'blocked_sport': 8080,
-    'comparison_sport': 8081,
-    'dport': 1234
-}
+tcp_packet_drop_probe = None
+# uncomment below if you have tcp port being blocked by GFW
+#tcp_packet_drop_probe = {
+#    'blocked_sport': 8080,
+#    'comparison_sport': 8081,
+#    'dport': 1234
+#}
 udp_packet_drop_probe = None
+# uncomment below if you have udp port being blocked by GFW
 #udp_packet_drop_probe = {
 #    'blocked_sport': 8080,
 #    'comparison_sport': 8081,
@@ -49,3 +55,6 @@ if os.path.exists(MONGOL_CFG_PATH):
     user_config = {}
     exec user_config_code in user_config
     sys.modules[__name__].__dict__.update(user_config)
+
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
